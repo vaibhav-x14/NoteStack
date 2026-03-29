ea7b9bc51ca33ad718ec151ba2b495d7e7f50871
vaibhav-x14 <vaibhavparjapat494@gmail.com>
2026-03-29 16:39:16 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true, // 🔥 important
   },
-  { timestamps: true }
-);
+  password: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
